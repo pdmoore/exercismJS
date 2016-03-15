242641b4695cecdf7c08419fd7eabfa8d52993d0
Paul Moore <pdmoore@gmail.com>
2016-03-14 22:38:08 -0700

    handle DNA strings of differing lengths

--- hamming/hamming.js
+++ hamming/hamming.js
@@ -1,5 +1,9 @@
 function Hamming() {
     this.compute = function(left, right) {
+        if (left.length !== right.length) {
+            throw "DNA strands must be of equal length.";
+        }
+
         var count = 0;
         for (i = 0; i < left.length; i++) {
             if (left.charAt(i) !== right.charAt(i)) count++;
